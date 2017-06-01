44bdf85130770fb268faa3c428aaf1889f3367a2
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-01 22:03:07 +0300

    create two controllers: pets~ and owners~

--- Pets.WebAPIApp/Controllers/ValuesController.cs
+++ Pets.WebAPIApp/Controllers/ValuesController.cs
@@ -1,12 +1,37 @@
-﻿namespace Pets.WebAPIApp.Controllers
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Pets.Models;
+
+namespace Pets.WebAPIApp.Controllers
 {
+    using Database;
     using System.Web.Http;
 
-    public class ValuesController : ApiController
+    public class OwnersController : ApiController
     {
         public IHttpActionResult GetValues()
         {
-            return Ok(new[] { "a", "b", "c" });
+            using (var context = new PetsDbContext())
+            {
+                PetOwner[] owners = context.Owners
+                    .Include(owner=>owner.Pets)
+                    .ToArray();
+
+                return Ok(owners);
+            }
+        }
+    }
+
+    public class PetsController : ApiController
+    {
+        public IHttpActionResult GetValues()
+        {
+            using (var context = new PetsDbContext())
+            {
+                Pet[] pets = context.Pets.Include(x=>x.Owner).ToArray();
+                return Ok(pets);
+            }
         }
     }
 }
