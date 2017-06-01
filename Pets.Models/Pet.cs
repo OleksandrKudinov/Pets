4840995213a38c153c4856931248b7fe4d9ecaf9
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-01 22:00:09 +0300

    create models project

---
+++ Pets.Models/Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pets.Models
+{
+    public class Pet
+    {
+        public Int32 PetId { get; set; }
+        public PetOwner Owner { get; set; }
+        public String Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public String KindOfAnimal { get; set; }
+    }
+}
---
+++ Pets.Models/PetOwner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pets.Models
+{
+    public class PetOwner
+    {
+        public Int32 PetOwnerId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public ICollection<Pet> Pets { get; set; }
+    }
+}
